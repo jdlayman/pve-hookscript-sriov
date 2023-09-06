01ae957dcf3af408d4e70f3500509d3b900a41bb
JD Layman <jlayman2@gmail.com>
2023-09-05 20:32:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Proxmox Hookscript for FDB Management
+As described in https://bugzilla.redhat.com/show_bug.cgi?id=1067802, when a linux bridge is attached to a physical function of a NIC that supports virtual functions using SR-IOV, MAC addresses for guests on the linux bridge are not automatically added to the forwarding database of the physical function's interface.
+
+This hookscript can be attached to a VM using the following command:
+`qm set \<vmid\> --hookscript local:snippets/bridgefix.sh`
+or a container:
+`pct set \<vmid\> --hookscript local:snippets/bridgefix.sh`
+
+This will invoke a hookscript placed in the default snippet location of:
+`/var/lib/vz/snippets/`
+
+## Implementation
+1. The script searches the LXC/VM `<vmid>.conf` within `/etc/pve` to find the MAC address of all configured interfaces of the guest attached to a bridge. 
+2. For each discovered bridge, the script looks for all interfaces connected to the bridge (linked within `/sys/class/net/<bridge>/brif/`.
+3. If the interface is a bond, individual slave interfaces are determined by inspecting `/sys/class/net/<bond>/bonding`.
+4. For each interface identified in steps #2 and #3, if the interface is a physical interface that also has virtual functions (as determined by the existence of links named  `/sys/class/net/<intf>/device/virtfn#`), the MAC address of the guest is added to the forwarding database for the interface using the following command: `bridge fdb add <mac-addr> dev <intf>`.
+
+### References
+- This script was originally provided by murky51 on the Proxmox forums: https://forum.proxmox.com/threads/communication-issue-between-sriov-vm-vf-and-ct-on-pf-bridge.68638/#post-448742
+- Slides 21 and 22 of https://netdevconf.info/0.1/docs/netdev_tutorial_bridge_makita_150213.pdf also describe the problem and solution
+
---
+++ bridgefix.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+# Hook script for PVE guests (hookscript config option)
+# You can set this via pct/qm with
+# pct set <vmid> -hookscript <volume-id>
+# qm set <vmid> -hookscript <volume-id>
+# where <volume-id> has to be an executable file in the snippets folder
+# of any storage with directories e.g.:
+# for KVM: qm set 100 -hookscript local:snippets/pf-bridge-fdb.sh
+# or
+# for CT: pct set 100 -hookscript local:snippets/pf-bridge-fdb.sh
+
+#
+# Modified from ctr's hookscript to add bridged CT and VM MAC addresses to the upstream PF interface.
+# https://forum.proxmox.com/threads/communication-issue-between-sriov-vm-vf-and-ct-on-pf-bridge.68638/#post-435959
+# 
+
+USAGE="Usage: $0 vmid phase"
+
+if [ "$#" -ne "2" ]; then
+  echo "$USAGE"
+  exit 1
+fi
+
+echo "GUEST HOOK: $0 $*"
+
+# First argument is the vmid
+
+vmid=$1
+if [[ $1 == ?(-)+([:digit:]) ]]; then
+  echo "$USAGE"
+  exit 1
+fi
+
+# Second argument is the phase
+
+phase=$2
+case "${phase}" in
+  pre-start|post-start|pre-stop|post-stop) : ;;
+  *)                                       echo "got unknown phase ${phase}"; exit 1 ;;
+esac
+
+
+function fixup_bridge_fdb {
+  OPERATION=$1
+  # Lookup Proxmox config for by vmid
+  CONFFILE=$(find /etc/pve -type f -name ${vmid}.conf)
+  if [ -f "${CONFFILE}" ]; then
+    # get defined networks
+    NETWORKS=$(egrep "^net" ${CONFFILE}| fgrep bridge= | awk '{print $2}')
+    #echo $NETWORKS
+    for i in ${NETWORKS}; do
+      #echo $i
+      declare macaddr=""
+      declare bridge=""
+      declare vlan=""
+      IFS=\, read -a NETWORK <<<"$i"
+      # get attributes for current network
+      for item in "${NETWORK[@]}"; do
+        IFS=\= read -a kv <<<"$item"
+        case "${kv[0]}" in 
+          tag)     vlan=${kv[1]};;
+          bridge)  bridge=${kv[1]};;
+          virtio)  macaddr=${kv[1]};;
+          hwaddr)  macaddr=${kv[1]};;
+        esac
+      done
+      # special processing needed if member of vlan
+      if [ ! -z "${vlan}" ]; then
+        vlancheck=${vlan}
+      else
+        vlancheck="checking"
+      fi
+      # lookup member interfaces of defined bridge interface
+      bridgeinterfaces=$(ls -1 /sys/class/net/${bridge}/brif/ 2>/dev/null)
+      # for every member interface, if it is an SR-IOV PF then ...
+      #echo $bridgeinterfaces
+      for memberint in ${bridgeinterfaces}; do
+	if [ -d "/sys/class/net/${memberint}/bonding" ]; then
+          # find interfaces that are bonded
+	  IFS=' ' read -a bondedints <<< $(cat /sys/class/net/${memberint}/bonding/slaves 2>/dev/null)
+          for bondedint in ${bondedints[@]}; do
+            #echo $bondedint
+            if [ -L "/sys/class/net/${bondedint}/device/virtfn0" ]; then
+              # echo $memberint
+              subint="${bondedint}"
+              # check if entry in fdb and only execute when needed
+              present=$(bridge fdb show dev ${subint} | fgrep -i ${macaddr})
+              if [[ -z $present && $OPERATION == "add" ]] || [[ -n $present && $OPERATION == "del" ]]; then
+                echo bridge fdb ${OPERATION} ${macaddr} dev ${subint}
+                bridge fdb ${OPERATION} ${macaddr} dev ${subint}
+              fi
+            fi
+          done
+        fi
+        # only proceed if memberinterface is PF with VF
+        if [ -L "/sys/class/net/${memberint}/device/virtfn0" ]; then
+          # echo $memberint
+          subint="${memberint}"
+          ## check if a vf is on the vlan
+          #vf=$(ip link show dev ${memberint} 2>/dev/null |awk -v vlan="${vlancheck}" '{ if( $6 ~ vlan) print $2}')
+          #if [ ! -z "${vf}" ]; then
+          #  if [ ! -z "${vlan}" ]; then
+          #    subint="${memberint}.${vlan}"
+          #  else
+          #    subint="${memberint}"
+          #  fi
+          #fi
+          # check if entry in fdb and only execute when needed
+          present=$(bridge fdb show dev ${subint} | fgrep -i ${macaddr})
+          if [[ -z $present && $OPERATION == "add" ]] || [[ -n $present && $OPERATION == "del" ]]; then
+            echo bridge fdb ${OPERATION} ${macaddr} dev ${subint}
+            bridge fdb ${OPERATION} ${macaddr} dev ${subint}
+          fi
+        fi
+      done
+    done
+  else
+    echo "VM or CT does not exist, aborting"
+  fi
+}
+
+case "${phase}" in
+  pre-start)  echo "${vmid} is starting, doing bridge fdb setup." && fixup_bridge_fdb add ;;
+  post-stop)  echo "${vmid} stopped. Doing bridge fdb cleanup." && fixup_bridge_fdb del ;;
+esac
+
+exit 0
+
